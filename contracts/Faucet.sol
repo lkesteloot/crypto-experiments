f12613471249eb5b6ad1cc44e94aa2527f51e870
Lawrence Kesteloot <lk@teamten.com>
2021-07-24 14:57:18 -0700

    Add function to Faucet.

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -6,7 +6,11 @@ contract Faucet {
         
         msg.sender.transfer(withdraw_amount);
     }
-    
+
+    function getBalance() view public returns (uint256 balance) {
+        return address(this).balance;
+    }
+
     function () external payable {
         // Nothing.
     }
